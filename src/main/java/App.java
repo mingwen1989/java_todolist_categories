c5056ba1c8f811d6bb74f77208e2a9db59571d3a
Sadio Ali <sadiali@pdx.edu>
2016-04-19 16:13:58 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,26 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
 
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String userInput = request.queryParams("number");
+      NumbersToWords newNumbers = new NumbersToWords();
+      String convertedNumber = newNumbers.integerConverter(userInput);
+      model.put("convertedNumber", convertedNumber);
+
+      model.put("template", "templates/detector.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+  }
+
+  public static String methodName(String inputVar) {}
+  
 }
