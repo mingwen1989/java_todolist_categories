66c7d3671d4f3db9010444ae782e14e1556a78aa
justin fokes and kendra dunham <j.k.fokes@gmail.com>
2016-04-27 10:12:50 -0700

    taDAAAAAAA

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,24 +12,36 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("tasks", request.session().attribute("tasks"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/tasks", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      ArrayList<Task> tasks = request.session().attribute("tasks");
-      if (tasks == null) {
-      tasks = new ArrayList<Task>();
-      request.session().attribute("tasks", tasks);
-      }
-      String description = request.queryParams("description");
-      Task newTask = new Task(description);
-      tasks.add(newTask);
-      model.put("template", "templates/success.vtl");
+    get("tasks/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    get("/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -31,28 +31,45 @@ public class AppTest extends FluentTest {
   @Test
   public void taskIsCreatedTest() {
   goTo("http://localhost:4567/");
+  click("a", withText("Add a new task"));
   fill("#description").with("Mow the lawn");
   submit(".btn");
   assertThat(pageSource()).contains("Your task has been saved.");
   }
   @Test
   public void taskIsDisplayedTest() {
-  goTo("http://localhost:4567/");
+  goTo("http://localhost:4567/tasks/new");
   fill("#description").with("Mow the lawn");
   submit(".btn");
-  click("a", withText("Go Back"));
+  click("a", withText("View tasks"));
   assertThat(pageSource()).contains("Mow the lawn");
 }
   @Test
   public void multipleTasksAreDisplayedTest() {
-  goTo("http://localhost:4567/");
+  goTo("http://localhost:4567/tasks/new");
   fill("#description").with("Mow the lawn");
   submit(".btn");
-  click("a", withText("Go Back"));
+  goTo("http://localhost:4567/tasks/new");
   fill("#description").with("Buy groceries");
   submit(".btn");
-  click("a", withText("Go Back"));
+  click("a", withText("View tasks"));
   assertThat(pageSource()).contains("Mow the lawn");
   assertThat(pageSource()).contains("Buy groceries");
   }
+  @Test
+  public void taskShowPageDisplaysDescription() {
+    goTo("http://localhost:4567/tasks/new");
+    fill("#description").with("Do the dishes");
+    submit(".btn");
+    click("a", withText("View tasks"));
+    click("a", withText("Do the dishes"));
+    assertThat(pageSource()).contains("Do the dishes");
+  }
+  @Test
+  public void taskNotFoundMessageShown() {
+    goTo("http://localhost:4567/tasks/999");
+    assertThat(pageSource()).contains("Task not found");
+  }
+
+
 }
