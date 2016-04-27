a432879ced28910aca4a5a9b115d4b16150fc20a
justin fokes and kendra dunham <j.k.fokes@gmail.com>
2016-04-27 09:22:59 -0700

    updated to do list with time and id finds

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
@@ -11,8 +12,48 @@ public class TaskTest {
   }
 
   @Test
-  public void Task_instantiatesWithDescription_String() {
-  Task myTask = new Task("Mow the lawn");
-  assertEquals("Mow the lawn", myTask.getDescription());
+    public void Task_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
   }
+  @Test
+    public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today(){
+    Task myTask = new Task("Mow the Lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+  @Test
+  public void all_returnsAllInstancesOfTask_true(){
+    Task firstTask = new Task("mow the lawn");
+    Task secondTask = new Task("buy groceries");
+    assertTrue(Task.all().contains(firstTask));
+    assertTrue(Task.all().contains(secondTask));
+  }
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    Task myTask = new Task("Mow the lawn");
+    Task.clear();
+    assertEquals(Task.all().size(), 0);
+  }
+@Test
+public void getId_tasksInstantiatesWithAnId_1(){
+  Task.clear();
+  Task myTask = new Task("mow the lawn");
+  assertEquals(1, myTask.getId());
+}
+@Test
+public void find_returnsTaskWithSameId_secondTask() {
+  Task firstTask = new Task("Mow the lawn");
+  Task secondTask = new Task("Buy groceries");
+  assertEquals(Task.find(secondTask.getId()), secondTask);
+}
+@Test
+ public void find_returnsNullWhenNoTaskFound_null() {
+   assertTrue(Task.find(999) == null);
+ }
+
 }
