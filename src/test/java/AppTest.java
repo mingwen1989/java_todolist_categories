6ad6c0d9da7ebdd5a65aeb6ea05a9cc7485c0c24
Justin Fokes and Ming Wen <mwen82@gmail.com>
2016-04-28 10:06:23 -0700

    update category

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
@@ -25,51 +24,8 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Task list!");
+    assertThat(pageSource()).contains("Todo list!");
+    assertThat(pageSource()).contains("View Category List");
+    assertThat(pageSource()).contains("Add a New Category");
   }
-
-  @Test
-  public void taskIsCreatedTest() {
-  goTo("http://localhost:4567/");
-  click("a", withText("Add a new task"));
-  fill("#description").with("Mow the lawn");
-  submit(".btn");
-  assertThat(pageSource()).contains("Your task has been saved.");
-  }
-  @Test
-  public void taskIsDisplayedTest() {
-  goTo("http://localhost:4567/tasks/new");
-  fill("#description").with("Mow the lawn");
-  submit(".btn");
-  click("a", withText("View tasks"));
-  assertThat(pageSource()).contains("Mow the lawn");
-}
-  @Test
-  public void multipleTasksAreDisplayedTest() {
-  goTo("http://localhost:4567/tasks/new");
-  fill("#description").with("Mow the lawn");
-  submit(".btn");
-  goTo("http://localhost:4567/tasks/new");
-  fill("#description").with("Buy groceries");
-  submit(".btn");
-  click("a", withText("View tasks"));
-  assertThat(pageSource()).contains("Mow the lawn");
-  assertThat(pageSource()).contains("Buy groceries");
-  }
-  @Test
-  public void taskShowPageDisplaysDescription() {
-    goTo("http://localhost:4567/tasks/new");
-    fill("#description").with("Do the dishes");
-    submit(".btn");
-    click("a", withText("View tasks"));
-    click("a", withText("Do the dishes"));
-    assertThat(pageSource()).contains("Do the dishes");
-  }
-  @Test
-  public void taskNotFoundMessageShown() {
-    goTo("http://localhost:4567/tasks/999");
-    assertThat(pageSource()).contains("Task not found");
-  }
-
-
 }
