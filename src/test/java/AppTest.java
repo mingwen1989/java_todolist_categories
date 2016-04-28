04beb976d730d5d31e1a1e178f3eba3bc656fddf
Justin Fokes and Ming Wen <mwen82@gmail.com>
2016-04-28 10:27:26 -0700

    update categories homework

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -28,4 +28,58 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("View Category List");
     assertThat(pageSource()).contains("Add a New Category");
   }
+  @Test
+  public void categoryIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Category"));
+    fill("#name").with("Household chores");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your category has been saved.");
+  }
+
+  @Test
+  public void categoryIsDisplayedTest() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Household chores");
+    submit(".btn");
+    click("a", withText("View categories"));
+    assertThat(pageSource()).contains("Household chores");
+  }
+
+  @Test
+  public void categoryShowPageDisplaysName() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Household chores");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Household chores"));
+    assertThat(pageSource()).contains("Household chores");
+  }
+
+  @Test
+  public void categoryTasksFormIsDisplayed() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Shopping");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Shopping"));
+    click("a", withText("Add a new task"));
+    assertThat(pageSource()).contains("Add a task to Shopping");
+  }
+
+  @Test
+  public void tasksIsAddedAndDisplayed() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Banking");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Banking"));
+    click("a", withText("Add a new task"));
+    fill("#description").with("Deposit paycheck");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Banking"));
+    assertThat(pageSource()).contains("Deposit paycheck");
+  }
+
 }
